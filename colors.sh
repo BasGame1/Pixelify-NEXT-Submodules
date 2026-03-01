d7214f80e41a7480f7f43b1d7966aa971b79087d
BasGame1 <abadbasantaalex@gmail.com>
2026-03-01 14:35:05 +0100

    colors: Init

---
+++ colors.sh
@@ -0,0 +1,29 @@
+red() {
+ echo -e "\033[0;31m"
+ echo "$1"
+ echo -e "\033[0m"
+}
+
+blue() {
+ echo -e "\033[0;34m"
+ echo "$1"
+ echo -e "\033[0m"
+}
+
+green() {
+ echo -e "\033[0;32m"
+ echo "$1"
+ echo -e "\033[0m"
+}
+
+yellow() {
+ echo -e "\033[1;33m"
+ echo "$1"
+ echo -e "\033[0m"	
+}
+
+purple() {
+ echo -e "\033[0;35m"
+ echo "$1"
+ echo -e "\033[0m"
+}
