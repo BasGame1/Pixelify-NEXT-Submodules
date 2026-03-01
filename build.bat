d3d8f74b6aef7bcc72396686a61a11197d4c6866
BasGame1 <abadbasantaalex@gmail.com>
2026-03-01 14:34:43 +0100

    build (bat): Init windows build system

---
+++ build.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal enabledelayedexpansion
+
+if not exist colors.bat (
+    echo error, could not load colors file
+)
+
+echo What submodule you want to build?
+call colors.bat red "CallScreen: 1"
+call colors.bat blue "GPhotos: 2"
+call colors.bat yellow "InstallAPK: 3"
+call colors.bat green "PixelLauncher: 4"
+call colors.bat purple "Systemazer: 5"
+echo All: press enter
+echo.
+
+set /p SELECTION="Enter your selection: "
+
+if "%SELECTION%"=="1" (
+    call colors.bat red "Building Call Screen"
+    call gradlew :CallScreen:buildZip --no-configuration-cache
+) else if "%SELECTION%"=="2" (
+    call colors.bat blue "Building GPhotos"
+    call gradlew :GPhotos:buildZip --no-configuration-cache
+) else if "%SELECTION%"=="3" (
+    call colors.bat yellow "Building InstallAPK"
+    call gradlew :InstallAPK:buildZip --no-configuration-cache
+) else if "%SELECTION%"=="4" (
+    call colors.bat green "Building PixelLauncher"
+    call gradlew :InstallPixelLauncher:buildZip --no-configuration-cache
+) else if "%SELECTION%"=="5" (
+    call colors.bat purple "Building Systemazer"
+    call gradlew :Systemazer:buildZip --no-configuration-cache
+) else (
+    echo Building everything
+    call gradlew buildAll --no-configuration-cache
+    echo Cleaning
+    call gradlew clean --no-configuration-cache
+)
+
+pause
