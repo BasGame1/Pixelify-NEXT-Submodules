0e1501c56cd1fec1c00e8d71dc546181d282bc2d
BasGame1 <abadbasantaalex@gmail.com>
2026-06-17 23:35:46 +0200

    bootanimation: Init new submodule

---
+++ bootanimation/module/customize.sh
@@ -0,0 +1,29 @@
+#/system/sh
+
+# Init the var with just a random value
+SYSTEM=true
+
+print "[INFO] Checking installation folder"
+
+if [ -f system/media/bootanimation.zip ]; then
+ SYSTEM=true
+elif [ -f product/media/bootanimation.zip ]; then
+ SYSTEM=false
+else
+ abort "[ERROR] bootanimation dir could not be found, aborting"
+fi
+
+case $SYSTEM in
+ true)
+  print "[INFO] bootanimation detected in system, moving files"
+  mkdir -p $MODPATH/system/media  
+  cp $MODPATH/bootanimation.zip $MODPATH/system/media  
+ ;;
+ false)
+  print "[INFO] bootanimation detected in product, moving files"
+  mkdir -p $MODPATH/product/media
+  cp $MODPATH/bootanimation.zip $MODPATH/product/media
+ ;;
+esac
+
+print "[SUCCESS] Bootanimation installed, enjoy it!"
