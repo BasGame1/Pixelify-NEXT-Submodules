7f2d56e912d28edc2b927981b900fe5d7e55dc2b
BasGame1 <abadbasantaalex@gmail.com>
2026-06-17 23:49:03 +0200

    gemini bootanimation: module: customize: (Again) change print for ui_print and fix absolute path

--- Gemini_bootanimation/module/customize.sh
+++ Gemini_bootanimation/module/customize.sh
@@ -3,11 +3,11 @@
 # Init the var with just a random value
 SYSTEM=true
 
-print "[INFO] Checking installation folder"
+ui_print "[INFO] Checking installation folder"
 
-if [ -f system/media/bootanimation.zip ]; then
+if [ -f /system/media/bootanimation.zip ]; then
  SYSTEM=true
-elif [ -f product/media/bootanimation.zip ]; then
+elif [ -f /product/media/bootanimation.zip ]; then
  SYSTEM=false
 else
  abort "[ERROR] bootanimation dir could not be found, aborting"
@@ -15,15 +15,15 @@ fi
 
 case $SYSTEM in
  true)
-  print "[INFO] bootanimation detected in system, moving files"
+  ui_print "[INFO] bootanimation detected in system, moving files"
   mkdir -p $MODPATH/system/media  
   cp $MODPATH/gemini-bootanimation.zip $MODPATH/system/media/bootanimation.zip
  ;;
  false)
-  print "[INFO] bootanimation detected in product, moving files"
+  ui_print "[INFO] bootanimation detected in product, moving files"
   mkdir -p $MODPATH/product/media
   cp $MODPATH/gemini-bootanimation.zip $MODPATH/product/media/bootanimation.zip
  ;;
 esac
 
-print "[SUCCESS] Gemini bootanimation installed, enjoy it!"
+ui_print "[SUCCESS] Gemini bootanimation installed, enjoy it!"
